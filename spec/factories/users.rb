8582dd4692f6e3ddfb9e0d0581cde5039ce92642
Abdullah Alger <abdullahalger@me.com>
2015-07-24 19:22:09 +0200

    added two capybara tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    name 'Pigeon Dan'
+    email 'test@example.com'
+    password 'helloworld'
+    password_confirmation "helloworld"
+    confirmed_at Time.now
   end
 
 end
---
+++ spec/features/wiki_create_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature '[[create wikis]]' do
+
+  before(:each) do
+    user = FactoryGirl.create(:user)
+    user.save
+    login_as(user, :scope => :user)
+  end
+
+  scenario "click the button to make a new wiki" do
+
+    visit "/wikis"
+    expect(current_path).to eq("/wikis")
+
+    click_link("Create Wiki")
+    expect(page).to have_content("Title" && "Wiki")
+  end
+
+  scenario "enter content to make a new wiki" do
+
+    visit "/wikis/new"
+    expect(current_path).to eq("/wikis/new")
+
+    fill_in :wiki_title, :with => "New Wiki Title"
+    fill_in :wiki_body, :with => "This is some content in the wiki."
+
+    click_button "Save"
+    expect(current_path).to eq('/wikis/1')
+    expect(page).to have_content "Wiki was saved."
+    expect(page).to have_content "New Wiki Title"
+  end
+
+end
