795e586cf9b52e0a6b6f88f9d106f243ced25370
Abdullah Alger <abdullahalger@me.com>
2015-07-29 23:09:38 +0200

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets.js
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/javascripts/bootstrap-sprockets.js)
 
