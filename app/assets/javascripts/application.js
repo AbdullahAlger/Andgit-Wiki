16d4b9ac4215acd9d94c84dffa76674ccaef326b
Abdullah Alger <abdullahalger@me.com>
2015-07-29 22:11:14 +0200

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sass
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets.js
 //= require_tree .
