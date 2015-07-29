3c69d5ac071ee3c1b228d07a6b306998a2c86f9a
Abdullah Alger <abdullahalger@me.com>
2015-07-29 21:58:47 +0200

    .

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
+# application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 # Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
 puts "-"*20
